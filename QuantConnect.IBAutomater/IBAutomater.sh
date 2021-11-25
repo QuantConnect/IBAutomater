eaa98d13b9ce3a81028b37d66e730b9af36ea9a3
NoviProg <80051122+NoviProg@users.noreply.github.com>
2021-11-25 21:08:06 +0300

    Update IBAutomater.sh

--- QuantConnect.IBAutomater/IBAutomater.sh
+++ QuantConnect.IBAutomater/IBAutomater.sh
@@ -4,10 +4,13 @@ ps -AFH
 
 pkill -9 Xvfb
 pkill -9 java
+pkill -9 socat
 
 sleep 5
 
 ps -AFH
+
+socat TCP4-LISTEN:`hostname -I | awk '{print $1}' | awk '{split($0,p,"."); print 4p[4]}'`,bind=`hostname -I | awk '{print $1}'`,fork,forever TCP:127.0.0.1:4001 &
 
 Xvfb :1 -screen 0 1024x768x24 2>&1 >/dev/null &
 export DISPLAY=:1
