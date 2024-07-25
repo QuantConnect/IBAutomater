9b2e6efcbd8194886e614df9f6581ca3c69f1070
Omid K. Rad <omidkrad@users.noreply.github.com>
2024-07-25 15:06:42 -0700

    Avoid IOException when log file is in use

--- QuantConnect.IBAutomater/IBAutomater.cs
+++ QuantConnect.IBAutomater/IBAutomater.cs
@@ -268,7 +268,14 @@ namespace QuantConnect.IBAutomater
                 {
                     if (File.Exists(_ibGatewayLogFileName))
                     {
-                        File.Delete(_ibGatewayLogFileName);
+                        try
+                        {
+                            File.Delete(_ibGatewayLogFileName);
+                        }
+                        catch (IOException ex)
+                        {
+                            
+                        }
                     }
                 }
 
